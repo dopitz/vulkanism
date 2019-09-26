0d9873fb5af532c1635e6a82607f419a3bc30997
dopitz <daniel.opitz@kit.edu>
2019-09-26 11:01:49 +0200

    gui: spawn multiple commands and command completion with empty string

--- nobs-imgui/src/shell/command/spawn.rs
+++ nobs-imgui/src/shell/command/spawn.rs
@@ -13,36 +13,44 @@ impl<S: 'static + Style, C: 'static + Clone + Send> Command<S, C> for Cmd<S, C> 
     (
       "run command asynchronously",
       concat!(
-        "spawn <cmd> <args..>\nSpawns a new thread that runs the command <cmd> with arguments <args..>\n",
+        "spawn <cmd> <args..> [--- <cmd> <args..> ..]\n",
+        "Spawns a new thread that runs the command <cmd> with arguments <args..>\n",
+        "Multiple commands separated with '---' may be passed to the new thread and executed in sequence.\n\n",
         "Arguments\n",
-        "<cmd>      - Name of the command to execute, accepts all commands that are registered with the shell\n",
-        "<args...>  - One or more, whitespace separated arguments\n"
+        "<cmd>     - Name of the command to execute, accepts all commands that are registered with the shell\n",
+        "<args..>  - One or more, whitespace separated arguments\n\n",
+        "Example\n",
+        "spawn cmd1 --- cmd2 a b c --- cmd3 x y\n",
+        "This will spawn a new thread that executes the three commands in sequence\n"
       ),
     )
   }
 
   fn run(&self, args: Vec<String>, shell: Shell<S, C>, context: &mut C) {
-    let cmd = if args.len() > 1 {
-      args[1..].iter().fold(args[0].clone(), |s, a| format!("{} \"{}\"", s, a))
-    } else {
-      args[0].clone()
-    };
-
     let name = self.get_name();
     let mut c = context.clone();
     std::thread::spawn(move || {
-      shell.exec(&cmd, &mut c);
-      shell.get_term().println(&format!("exiting {} {}", name, args[0]))
+      let term = shell.get_term();
+      for a in args {
+        term.println(&a);
+        shell.exec(&a, &mut c);
+      }
+      term.println("exiting")
     });
   }
 
   fn parse(&self, s: &str) -> Option<Vec<String>> {
     if s.starts_with(self.get_name()) {
-      s.chars()
-        .skip(self.get_name().len())
-        .position(|c| c != ' ')
-        .map(|p| &s[p + self.get_name().len()..])
-        .and_then(|s| self.cmds.iter().find_map(|c| c.parse(s)))
+      let s = s[self.get_name().len()..]
+        .split("---")
+        .map(|s| s.trim().to_string())
+        .filter(|s| self.cmds.iter().find(|c| c.parse(&s).is_some()).is_some())
+        .collect::<Vec<_>>();
+      if s.is_empty() {
+        None
+      } else {
+        Some(s)
+      }
     } else {
       None
     }
@@ -51,12 +59,21 @@ impl<S: 'static + Style, C: 'static + Clone + Send> Command<S, C> for Cmd<S, C> 
     if self.get_name().starts_with(s) {
       Some(vec![arg::Completion::new(0, self.get_name().into())])
     } else if s.starts_with(self.get_name()) {
+      let len = s.rfind("---").map(|p| p + 3).unwrap_or(self.get_name().len());
       s.chars()
-        .skip(self.get_name().len())
+        .skip(len)
         .position(|c| c != ' ')
-        .map(|p| &s[p + self.get_name().len()..])
-        .and_then(|s| self.cmds.iter().find_map(|c| c.complete(s)))
-        .map(|c| c.into_iter().map(|c| c.prefix("spawn ".to_string())).collect())
+        .or_else(|| if s.len() > len { Some(s.len() - len) } else { None })
+        .map(|p| (&s[..p + len], &s[p + len..]))
+        .map(|(prefix, s)| {
+          self
+            .cmds
+            .iter()
+            .filter_map(|c| c.complete(s))
+            .flatten()
+            .map(|c| c.prefix(prefix.to_string()))
+            .collect::<Vec<_>>()
+        })
     } else {
       None
     }
